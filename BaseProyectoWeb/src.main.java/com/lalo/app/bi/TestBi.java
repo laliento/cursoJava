cadb9c0de8b0508c1f8ddcddc7d1095fb253b7ed
Eduardo Cruz Zamorano <Eduardo Cruz Zamorano@laloHost>
2017-03-11 21:34:14 -0600

    Creacióon de proyectos package

---
+++ BaseProyectoWeb/src.main.java/com/lalo/app/bi/TestBi.java
@@ -0,0 +1,8 @@
+package com.lalo.app.bi;
+/**
+ * @author Eduardo Cruz Zamorano
+ *
+ */
+public class TestBi {
+
+}
---
+++ BaseProyectoWeb/src.main.java/com/lalo/app/dao/TestDao.java
@@ -0,0 +1,8 @@
+package com.lalo.app.dao;
+/**
+ * @author Eduardo Cruz Zamorano
+ *
+ */
+public class TestDao {
+
+}
