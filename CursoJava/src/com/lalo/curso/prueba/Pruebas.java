c3e78d5975a120af3f27501e1edccce418ee0550
Eduardo Cruz Zamorano <Eduardo Cruz Zamorano@laloHost>
2017-05-27 15:46:24 -0500

    commmit inicial

---
+++ CursoJava/src/com/lalo/curso/prueba/Pruebas.java
@@ -0,0 +1,17 @@
+package com.lalo.curso.prueba;
+/**
+ * @author Eduardo Cruz Zamorano
+ *
+ */
+public class Pruebas {
+
+	/**
+	 * @param args
+	 * @author Eduardo Cruz Zamorano
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
